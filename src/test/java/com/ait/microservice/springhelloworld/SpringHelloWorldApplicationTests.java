77a7d3d6fd85fb0f479a98c8f993ec0d78069e86
Adrian O Sullivan <61162963+Adrian-osullivan@users.noreply.github.com>
2022-02-13 13:31:50 +0000

    fixing tests not running

--- src/test/java/com/ait/microservice/springhelloworld/SpringHelloWorldApplicationTests.java
+++ src/test/java/com/ait/microservice/springhelloworld/SpringHelloWorldApplicationTests.java
@@ -1,14 +1,24 @@
 package com.ait.microservice.springhelloworld;
 
 
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
 class SpringHelloWorldApplicationTests {
+    HelloService helloService;
+    @Before
+    public void setUp() throws Exception {
+        helloService = new HelloService();
+
+    }
 
     @Test
-    void contextLoads() {
+    public void test() {
+        assertEquals("Hello World 2022...", helloService.index());
     }
 
 }
